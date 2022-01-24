f2f2168758c85f109c5f17ba3226293e5cedbb4d
rashmi3168 <98230297+rashmi3168@users.noreply.github.com>
2022-01-25 00:04:17 +0530

    Add files via upload

---
+++ MissingElementInAnArray.java
@@ -0,0 +1,26 @@
+package week1.day2.classroom.arrays;
+import java.util.Arrays;
+
+public class MissingElementInAnArray {
+	public static void main(String[] args)
+	{
+		int[] arr= {1,2,3,4,7,6,8};
+		Arrays.sort(arr);   
+		System.out.println("Elements of array in ascending order: ");
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);  
+		}
+		
+		
+		for (int i = arr[0]; i < arr.length; i++)   
+		{       
+			
+			if (i!=arr[i-1])
+			{
+				System.out.println("Missing number in Array"+ i);
+				break;
+			}
+		}
+		
+	}
+}
